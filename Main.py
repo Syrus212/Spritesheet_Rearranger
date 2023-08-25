13643720089810e094074767d24f6229a343e9b8
Syrus12200 <syrus212.dev@gmail.com>
2023-08-25 11:37:59 +0200

    Fix anchor point instructions

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ root.withdraw()
 
 file = filedialog.askopenfilename(title='Select the spritesheet to rearrange.')
 
-anchor = numpy.clip(easygui.integerbox('choose the sprite anchor (from 1 to 9 starting from the top-left hand corner going clockwise)'), 1, 9)
+anchor = numpy.clip(easygui.integerbox('choose the sprite anchor (from 1 to 9 starting from the top-left hand corner going from left and right and then frop top to bottom)'), 1, 9)
 
 path = file[:file.rfind('/') + 1]
 
